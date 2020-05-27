9058b939649a83e91f3b3abac125fca24e0204ef
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-05-27 06:14:38 -0700

    Update test-sizeof-function.c

--- c-dump/test-sizeof-function.c
+++ c-dump/test-sizeof-function.c
@@ -3,29 +3,32 @@
 int main()
 {
 /*
+8
+8
 1
 8
-1
-4
-8
-8
-8
-8
-4
-4
 2
+4
+4
+8
+4
+8
+8
+1
+8
 */
-  printf("%d\n", sizeof(unsigned char));
-  printf("%d\n", sizeof(double));
+  printf("%d\n", sizeof(long signed int));
+  printf("%d\n", sizeof(long unsigned int));
   printf("%d\n", sizeof(char));
+  printf("%d\n", sizeof(unsigned long long));
+  printf("%d\n", sizeof(short));
+  printf("%d\n", sizeof(int));
   printf("%d\n", sizeof(unsigned int));
   printf("%d\n", sizeof(long long));
-  printf("%d\n", sizeof(unsigned long long));
-  printf("%d\n", sizeof(unsigned long));
-  printf("%d\n", sizeof(long));
-  printf("%d\n", sizeof(int));
   printf("%d\n", sizeof(float));
-  printf("%d\n", sizeof(short));
+  printf("%d\n", sizeof(double));
+  printf("%d\n", sizeof(unsigned long));
+  printf("%d\n", sizeof(unsigned char));
+  printf("%d\n", sizeof(long));
   return 0;
 }
-
