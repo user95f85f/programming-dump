ef5c7b25db1f9773fd3d72cb7ef6b83046735651
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-05-27 05:52:59 -0700

    Create test-sizeof-function.c

---
+++ c-dump/test-sizeof-function.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+/*
+1
+8
+1
+4
+8
+8
+8
+8
+4
+4
+2
+*/
+  printf("%d\n", sizeof(unsigned char));
+  printf("%d\n", sizeof(double));
+  printf("%d\n", sizeof(char));
+  printf("%d\n", sizeof(unsigned int));
+  printf("%d\n", sizeof(long long));
+  printf("%d\n", sizeof(unsigned long long));
+  printf("%d\n", sizeof(unsigned long));
+  printf("%d\n", sizeof(long));
+  printf("%d\n", sizeof(int));
+  printf("%d\n", sizeof(float));
+  printf("%d\n", sizeof(short));
+  return 0;
+}
+
